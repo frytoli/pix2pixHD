8e37ca893ce06e9fa8fb752d21cb8d81b050c6ba
frytoli <>
2022-05-05 17:00:56 -0500

    changes from WenliangDu/pix2pixHD to handle grayscale input images

--- test.py
+++ test.py
@@ -29,12 +29,12 @@ if not opt.engine and not opt.onnx:
         model.half()
     elif opt.data_type == 8:
         model.type(torch.uint8)
-            
+
     if opt.verbose:
         print(model)
 else:
     from run_engine import run_trt_engine, run_onnx
-    
+
 for i, data in enumerate(dataset):
     if i >= opt.how_many:
         break
@@ -49,16 +49,18 @@ for i, data in enumerate(dataset):
         assert opt.export_onnx.endswith("onnx"), "Export model file should end with .onnx"
         tensor_in = torch.Tensor(data['label'])
         torch.onnx.export(model, tensor_in,
-                          opt.export_onnx, verbose=True)
+                          opt.export_onnx, verbose=True, opset_version=13,
+                          input_names = ['input'], output_names = ['output'],
+                          do_constant_folding=True)
         exit(0)
-    minibatch = 1 
+    minibatch = 1
     if opt.engine:
         generated = run_trt_engine(opt.engine, minibatch, [data['label'], data['inst']])
     elif opt.onnx:
         generated = run_onnx(opt.onnx, opt.data_type, minibatch, [data['label'], data['inst']])
-    else:        
+    else:
         generated = model.inference(data['label'], data['inst'], data['image'])
-        
+
     visuals = OrderedDict([('input_label', util.tensor2label(data['label'][0], opt.label_nc)),
                            ('synthesized_image', util.tensor2im(generated.data[0]))])
     img_path = data['path']
